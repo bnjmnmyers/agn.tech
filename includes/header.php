76ad7bc69e4e1e98dd15617b5df9cca5df12e462
Benjamin Myers <bnjmn.myers@gmail.com>
2016-01-27 20:14:17 -0500

    Adding nav functionality

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <div id="header">
 	<img id="logo" src="images/logo.png" width="304" alt="Alchemy Global Networks Logo" title="Alchemy Global Networks Logo" />
     <nav id="nav">
-    	<a href="index.php">HOME</a>
-        <a href="services.php">SERVICES</a>
-        <a href="about.php">ABOUT</a>
-        <a href="contact.php">CONTACT</a>
+    	<a id="navHome" href="index.php">HOME</a>
+        <a id="navServices" href="services.php">SERVICES</a>
+        <a id="navAbout" href="about.php">ABOUT</a>
+        <a id="navContact" href="contact.php">CONTACT</a>
     </nav>
 </div>
---
+++ scripts/jquery.nav.js
@@ -0,0 +1,45 @@
+// JavaScript Document
+$(document).ready(function(){
+	var inTesting = true;
+	var pathname = window.location.pathname;
+	var devDir = '';
+	console.log(pathname);
+	if(inTesting){
+		devDir = '/dev';	
+	}
+	else{
+		devDir = '';	
+	}
+	switch(pathname){
+		case devDir+'/about.php':
+			$('#navAbout').addClass('active');
+			break;	
+		case devDir+'/contact.php':
+			$('#navContact').addClass('active');
+			break;
+		case devDir+'/dates.php':
+			$('#navService').addClass('active');
+			break;
+		default:
+			$('#navHome').addClass('active');	
+	}
+	
+	$('.active').css({
+		'color':'#dc9d34',
+		'border-bottom':'6px solid #dc9d34'
+	});
+	
+	$('#nav a').bind('mouseover',function(){
+		$(this).css({
+			'color':'#dc9d34',
+			'border-bottom':'6px solid #dc9d34'
+		});	
+	}).bind('mouseout',function(){
+		if(!$(this).hasClass('active')){
+			$(this).css({
+				'color':'#dc9d34',
+				'border-bottom':'none'
+			});
+		}
+	});
+});
