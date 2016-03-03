9d1dab33e50c4da1a524e2b094f62146d38e4e09
Benjamin Aaron Myers <bmyers@appguys.biz>
2016-03-02 20:00:09 -0500

    Working on index page styling Removed global.css global.less is now imported into each less file.

--- index.php
+++ index.php
@@ -6,16 +6,19 @@
     <!--[if lt IE 9]>
       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-    <link href="css/global.css" rel="stylesheet" />
+    <link href="css/index.css" rel="stylesheet" />
     <?php include("includes/scriptIncludes.php"); ?>
 </head>
 
 <body>
 	<div id="outer-wrapper">
     	<?php include('includes/header.php') ?>
-        <div id="slider">
-        	<img src="images/hacked.png" width="1366" alt="Network Security Image" title="Don't get hacked! Get Alchemy Global Networks, the gold standard in network security." />
-        </div>
+        <section id="showcase">
+        	<div id="container">
+            	<h1>Alchemy Global Networks</h1>
+                <h2>"The Gold Standard for Network Engineering"</h2>
+            </div>
+        </section>
         <div id="content-wrapper">
         </div>
         <?php include('includes/footer.php') ?>
