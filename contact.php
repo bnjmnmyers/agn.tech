7dc12c4a3015129021dbe146b6d7eb42b51def31
Benjamin Aaron Myers <bmyers@appguys.biz>
2016-03-12 11:44:37 -0500

    Working on contact form.

--- contact.php
+++ contact.php
@@ -18,36 +18,41 @@
         </div>
         <div id="content-wrapper">
         	<div id="content">
-            	<div id="contact-blurb" class="col-lg-6">
+            	<div id="contact-blurb" class="col-md-6">
                     <p>Your journey toward a more secure, robust, and functional network begins with a first step. Whether you need a quick answer to your urgent support needs or a long-term staff augmentation, the engineers at Alchemy Global Networks are here to help.</p>
         
                     <p>Complete the form below to learn more and get started. We’ll be in touch promptly with answers to your burning questions and information about the next steps.</p>
                 </div>
-                <div id="contact-box" class="col-lg-6">
+                <div id="contact-box" class="col-md-6">
                 	<div class="google-maps">
                     	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3339.54381856306!2d-83.3949088!3d36.1760251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885b918dcf36908f%3A0x852bf9bbe99ba7e0!2s6161+W+Andrew+Johnson+Hwy%2C+Talbott%2C+TN+37877!5e0!3m2!1sen!2sus!4v1457224428662" width="600" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
                     </div>
                 </div>
                 <form method="post" action="">
-                    <div class="col-lg-6">
+                	<input type="hidden" name="honeyPot" value="" />
+                    <div class="col-md-6">
                         <div class="input-row">
-                            <input type="text" name="first-name" placeholder="First Name" />
+                            <input type="text" name="first-name" placeholder="First Name*" />
                         </div>
                         <div class="input-row">
-                            <input type="text" name="last-name" placeholder="Last Name" />
+                            <input type="text" name="last-name" placeholder="Last Name*" />
                         </div>
                         <div class="input-row">
-                            <input type="text" name="phone" placeholder="Phone" />
+                            <input type="text" name="phone" placeholder="Phone*" />
                         </div>
                         <div class="input-row">
-                            <input type="text" name="email" placeholder="Email" />
+                            <input type="text" name="email" placeholder="Email*" />
                         </div>
+                        <div class="input-row">
+                            <input type="text" name="confirmEmail" placeholder="Confirm Email*" />
+                        </div>
+                    </div>
+                    <div class="col-md-6">
                         <div class="input-row">
                             <input type="text" name="company" placeholder="Company Name" />
                         </div>
-                    </div>
-                    <div class="col-lg-6">
                         <div class="input-row textarea-container">
+                        	<input type="hidden" name="requestType" value="1" />
                             <textarea name="request" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required" aria-required="true" aria-invalid="false" placeholder="Your Message"></textarea>
                         </div>
                     </div>
--- includes/functions.php
+++ includes/functions.php
@@ -2,4 +2,87 @@
 	
 	include 'includes/config.php';
 	
+	function processContact()
+	{
+		$firstname = $_REQUEST['first-name'];
+		$lastname = $_REQUEST['last-name'];
+		$phone = $_REQUEST['phone'];
+		$email = $_REQUEST['email'];
+		$confirmEmail = $_REQUEST['confirmEmail'];
+		$companyName = $_REQUEST['company'];
+		$requestType = $_REQUEST['requestType'];
+		$request = $_REQUEST['request'];
+		if($_REQUEST['honeyPot'] == '')
+		{
+			if($firstname != '')
+			{
+				if($lastname != '')
+				{
+					if($email != '' && ($email == $confirmEmail))
+					{
+						if($phone != '')
+						{
+							mysql_connect(AGNSQL1_CONNECTION, AGNSQL1_USER, AGNQL1_PASS);
+							if(mysql_ping())
+							{
+								mysql_select_db(FSSQL1_HOST);
+								$result = mysql_query("CALL getAllInstructors()") or die("Query fail: " . mysql_error());
+								
+								return $result;
+								
+								mysql_close();
+							}
+				
+							$rows = mysql_affected_rows();
+							
+							if($rows > 0)
+							{
+											
+								if($contactMethod == '')
+								{
+									$contactMethod = 'n/a';	
+								}
+								
+								$to = "info@ziondance.com";
+								$subject = "Zion Inquiry";
+								$message = "Booking: ".$booking.
+								"\r\nProposed Date: ".$proposedDate.
+								"\r\nPhone: ".$phone.
+								"\r\nPrefered Contact Method: ".$contactMethod.
+								"\r\nComments: ".$comments;
+								
+								$header = "From: ".$firstname.' '.$lastname.' '.'<'.$email.'>';
+								
+								mail($to,$subject,$message,$header);
+														
+								$processMessage = "Success";
+								return $processMessage;
+							}
+						}
+						else
+						{
+							$processMessage = "Please enter a valid phone number.";
+							return $processMessage;
+						}
+					}
+					else
+					{
+						$processMessage = "Please enter a valid email.";
+						return $processMessage;	
+					}
+				}
+				else
+				{
+					$processMessage = "Please enter a lastname.";
+					return $processMessage;
+				}
+			}
+			else
+			{
+				$processMessage = "Please enter a firstname.";
+				return $processMessage;
+			}
+		}
+	}
+	
 ?>
